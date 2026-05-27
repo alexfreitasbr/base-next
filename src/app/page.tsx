363283924bf78c5ac4be0114c0ee3a8f0ebf08bf
alexfreitasbr <alexfreitasbr@hotmail.com>
2026-05-27 15:14:17 -0300

    new setup tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex-1 flex-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
@@ -47,6 +47,7 @@ export default function Home() {
               alt="Vercel logomark"
               width={16}
               height={16}
+              style={{ height: 'auto' }}
             />
             Deploy Now
           </a>
