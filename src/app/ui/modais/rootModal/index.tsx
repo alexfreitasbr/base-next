2810a0e315706ed88f73b3bb99c601dc2d8404bf
alex freitas <alexfreitasbr@hotmail.com>
2026-05-27 20:54:40 -0300

    closeicon

--- src/app/ui/modais/rootModal/index.tsx
+++ src/app/ui/modais/rootModal/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { modalStore } from "@/store/modalStore";
 import { cn } from "@/lib/utilsTailWind";
+import { CircleX } from "lucide-react";
 
 export const RootModal = () => {
   const { modal, ariaLabelledby, ariaDescribedby,setModal } = modalStore();
@@ -17,8 +18,12 @@ export const RootModal = () => {
       )}
       onClick={() => setModal(null)}
     >
-      <div className="shrink rounded-lg bg-white p-6 shadow-xl">
-        {modal} <button onClick={() => setModal(null)}>x</button>
+      <div className="shrink rounded-lg bg-white p-6 shadow-xl">  
+                {modal}
+
+        <button onClick={() => setModal(null)}>
+          <CircleX className="w-5 h-5  transition-transform duration-200 hover:rotate-180" />
+        </button>
       </div>
     </div>
   );
